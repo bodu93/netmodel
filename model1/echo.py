1ad7cceecde7daea8b88864de46fba60fb1d29ca
bodu93 <bodu93@outlook.com>
2018-04-18 18:36:17 +0800

    model 1

---
+++ model1/echo.py
@@ -0,0 +1,19 @@
+from socketserver import BaseRequestHandler, TCPServer
+from socketserver import ForkingTCPServer, ThreadingTCPServer
+
+class EchoHandler(BaseRequestHandler):
+    def handle(self):
+        print("got connection from", self.client_address)
+        while True:
+            data = self.request.recv(4096)
+            if data:
+                sent = self.request.sendall(data)
+            else:
+                print("disconnect", self.client_address)
+                self.request.close()
+                break
+
+listen_address = ('', 2018)
+if __name__ == "__main__":
+    server = ForkingTCPServer(listen_address, EchoHandler)
+    server.serve_forever()
